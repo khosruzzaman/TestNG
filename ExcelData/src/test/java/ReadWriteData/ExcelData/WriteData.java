181728aa441a54a512e804758a3246d92a89b33c
khosruzzaman <preparation.qa@email.com>
2021-12-15 21:30:34 -0500

    Read Data

--- ExcelData/src/test/java/ReadWriteData/ExcelData/WriteData.java
+++ ExcelData/src/test/java/ReadWriteData/ExcelData/WriteData.java
@@ -1,70 +1,74 @@
 package ReadWriteData.ExcelData;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.poi.hslf.model.Sheet;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
 
-
 public class WriteData {
-	
-	@Test
+
+	 @Test
 	public void writeData() {
 		// Blank Workbook
 		XSSFWorkbook workbooks = new XSSFWorkbook();
-		
-		//Blank WorkSheet
-		XSSFSheet worksheets =workbooks.createSheet("Employee Data");
-		
+
+		// Blank WorkSheet
+		XSSFSheet worksheets = workbooks.createSheet("Employee Data");
+
 		// this data need to be use or written in (Object[])
 		Map<String, Object[]> data = new HashMap<String, Object[]>();
-		
-		//Insert Data 
-		
-		data.put("1", new Object[] {"ID", "FIRST-NAME", "LAST-NAME", "Email Address"});
-		data.put("2", new Object[] {"01", "Mohsin", "Khan", "nazmin@gmail.com"});
-		data.put("3", new Object[] {"06", "Hasan", "Khan"});
-		data.put("4", new Object[] {"65", "Shumon", "Ahmed", "sumon@gmail.com"});
-		data.put("5", new Object[] {"99", "Taher", "Ahmed"});
-		data.put("6", new Object[] {"77", "Miraz", "Zaman", "Miraz@gmail.com"});
-		
-		//Iterate over data and write to the sheet
-		
+
+		// Insert Data
+
+		data.put("1", new Object[] { "ID", "FIRST-NAME", "LAST-NAME", "Email Address" });
+		data.put("2", new Object[] { "01", "Mohsin", "Khan", "nazmin@gmail.com" });
+		data.put("3", new Object[] { "06", "Hasan", "Khan" });
+		data.put("4", new Object[] { "65", "Shumon", "Ahmed", "sumon@gmail.com" });
+		data.put("5", new Object[] { "99", "Taher", "Ahmed" });
+		data.put("6", new Object[] { "77", "Miraz", "Zaman", "Miraz@gmail.com" });
+
+		// Iterate over data and write to the sheet
+
 		Set<String> keyset = data.keySet();
 		int rowNum = 0;
-		
+
 		// Iterate data (Key) by help of Enhanced For Loop
-		
-		for(String keys : keyset) {
-			
-			//Create row for our Excelsheet
-			
+
+		for (String keys : keyset) {
+
+			// Create row for our Excelsheet
+
 			XSSFRow row = worksheets.createRow(rowNum++);
-			
+
 			// to get value for particuler key
 			Object[] objArr = data.get(keys);
-			
+
 			int cellNum = 0;
-			
-			// Iterate data (value) by help of Enhanced For Loop	
-			for(Object cellObj : objArr) {
+
+			// Iterate data (value) by help of Enhanced For Loop
+			for (Object cellObj : objArr) {
 				Cell cell = row.createCell(cellNum++);
-				//--
-				if(cellObj instanceof String) {
-					cell.setCellValue((String)cellObj);
-				}else if (cellObj instanceof Integer) {
-					cell.setCellValue((Integer)cellObj);
+				// --
+				if (cellObj instanceof String) {
+					cell.setCellValue((String) cellObj);
+				} else if (cellObj instanceof Integer) {
+					cell.setCellValue((Integer) cellObj);
 				}
-				//--/Users/khosruzzaman/eclipse-workspace/ExcelData/
+				// --/Users/khosruzzaman/eclipse-workspace/ExcelData/
 			}
-			
 		}
 		try {
 			// Location path for excel sheet
@@ -75,10 +79,25 @@ public class WriteData {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-		
 	}
-	
 
+	@Test
+	public void readDAta()   {
+		
+		try {
+			// Location path for excel sheet
+			FileInputStream fis = new FileInputStream("./Data/MyData.xls");
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
 }
+
+/*
+ * */
